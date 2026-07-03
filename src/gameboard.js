396e04440b1eea472293d53effca414ffea8d859
rootankush <rootankushprompt@gmail.com>
2026-07-03 15:50:37 +0530

    feat: recieveAttack function added

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,3 +1,82 @@
-import { Ship } from "./ship";
+import Ship from "./ship.js";
 
-class Gameboard {}
+const Gameboard = () => {
+	const missedAttacks = [];
+	const ships = [];
+
+	const placeShip = (startCoordinates, orientation, length) => {
+		const startX = startCoordinates[0];
+		const startY = startCoordinates[1];
+		const footprint = [];
+		if (startX > 9 || startX < 0) {
+			return false;
+		} else if (startY > 9 || startY < 0) {
+			return false;
+		}
+
+		if (orientation.toLowerCase() === "horizontal") {
+			for (let i = 0; i < length; i++) {
+				const currentX = startX + i;
+				const currentY = startY;
+
+				footprint.push([currentX, currentY]);
+			}
+		} else if (orientation.toLowerCase() === "vertical") {
+			for (let i = 0; i < length; i++) {
+				const currentX = startX;
+				const currentY = startY + i;
+
+				footprint.push([currentX, currentY]);
+			}
+		}
+
+		const newShip = Ship(length);
+
+		ships.push({
+			shipInstance: newShip,
+			coordinates: footprint,
+		});
+	};
+
+	const receiveAttack = (attackCoordinates) => {
+		const coordinatesOfShip = ships.find((shipObj) => {
+			let checkCord = shipObj.coordinates;
+			for(let i=0; i<checkCord.length; i++) {
+				if(checkCord[i][0] === attackCoordinates[0] && checkCord[i][1] === attackCoordinates[1]) {
+					return true;
+				}
+			}
+			return false;
+		})
+
+		if(coordinatesOfShip) {
+			let ship = coordinatesOfShip.shipInstance;
+			ship.hit();
+			return true;
+		}
+		else {
+			missedAttacks.push(attackCoordinates);
+			return false
+		}
+	};
+
+	const allShipsSunk = () => {};
+
+	const getMissedAttacks = () => {
+		return missedAttacks;
+	};
+
+	const getShipCoordinates = () => {
+		return ships.flatMap(shipObj => shipObj.coordinates);
+	};
+
+	return {
+		placeShip,
+		receiveAttack,
+		allShipsSunk,
+		getMissedAttacks,
+		getShipCoordinates,
+	};
+};
+
+export default Gameboard;
---
+++ src/gameboard.test.js
@@ -0,0 +1,33 @@
+import Gameboard from "./gameboard.js";
+import Ship from "./ship.js";
+
+describe("Gameboard", () => {
+	test("successfully places a horizontal ship within bounds", () => {
+		const gameboard = Gameboard();
+
+		gameboard.placeShip([0, 0], "horizontal", 3);
+
+		expect(gameboard.getShipCoordinates()).toEqual([
+			[0, 0],
+			[1, 0],
+			[2, 0],
+		]);
+	});
+	test("successfully hit the ship", () => {
+		const gameboard = Gameboard();
+		const ship = Ship();
+
+		gameboard.placeShip([0, 0], "horizontal", 3);
+
+		ship.hit();
+
+		expect(gameboard.receiveAttack([0, 0])).toEqual(true);
+	})
+	test.skip("get coordinates", () => {
+		const gameboard = Gameboard();
+
+		gameboard.placeShip([2,3], "horizontal", 3);
+
+		expect(gameboard.getShipCoordinates()).toEqual([[2, 3], [3, 3], [4, 3], ]);
+	})
+});
